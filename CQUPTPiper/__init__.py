00c73ef333ed210870cf07e482fe2a71d9b66b0c
Mivinci <1366723936@qq.com>
2019-09-14 23:17:57 +0800

    change >>> back to >. FUCK

--- CQUPTPiper/__init__.py
+++ CQUPTPiper/__init__.py
@@ -7,7 +7,7 @@ __version__ = '0.0.1'
 __year__ = '2019.9'
 __description__ = f'{__piper__} v{__version__} @{__author__} {__year__}'
 
-SHELL_PREFIX = '\033[92m>>>\033[0m '
+SHELL_PREFIX = '\033[92m>\033[0m '
 
 PIPER_DIR = f'{Path.home()}/.piper'
 PIPER_PATH = f'{PIPER_DIR}/piper'
